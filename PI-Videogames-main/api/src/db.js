b006a0cc6d7932e9ff9f41a3cdfc05c95eaebc2a
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2022-12-19 16:43:01 -0600

    BDD realizada videogames - genres y relacional, servidor corriendo

--- PI-Videogames-main/api/src/db.js
+++ PI-Videogames-main/api/src/db.js
@@ -5,11 +5,16 @@ const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname')
+// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}:${DB_HOST}/videogames`, {
+//   logging: false, // set to console.log to see the raw SQL queries
+//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// });
+const sequelize = new Sequelize('videogames', DB_USER, DB_PASSWORD, {
+  host: 'localhost',
+  dialect: /* one of 'mysql' | */'postgres'/* | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */
 });
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -30,10 +35,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { Videogame, Genres } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+Genres.belongsToMany(Videogame, {through: 'videogame_genre'});
+Videogame.belongsToMany(Genres, {through: 'videogame_genre'});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
