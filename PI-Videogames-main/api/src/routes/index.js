24b4e93fad39cc470bb4150c7b266e28bc2a3a41
Alberto Guerrero Covarrubias <a.guerrero.c.83@gmail.com>
2022-12-23 01:19:56 -0600

    prueba de rutas mediante alternativa 2.0

--- PI-Videogames-main/api/src/routes/index.js
+++ PI-Videogames-main/api/src/routes/index.js
@@ -1,12 +1,17 @@
-const { Router } = require('express');
+const express = require('express');
+// const { Router } = require('express');
+const router = express.Router();
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 
-const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.get('/',(req, res) => {
+    res.send('<h1>Welcome Player 1</h1><br/><h3>Press Start</h3><iframe width="560" height="315" src="https://www.youtube.com/embed/qFmwRriNJWs?start=715" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>');
+})
 
 
 module.exports = router;
